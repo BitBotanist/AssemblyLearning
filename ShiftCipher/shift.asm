252f58e537aa168974a44cabf5a12f0a958fa70f
BitBotanist <joe.nadeau1@gmail.com>
2025-12-28 17:05:45 -0500

    added logic for inside the loop. Need to debug as it's still just printing the plaintext at the end of the program. Possbly not going throgh loop properly or not writting ciphertext to buffer.

--- ShiftCipher/shift.asm
+++ ShiftCipher/shift.asm
@@ -7,14 +7,14 @@ section .bss
 section .data
 	inputMsg: db 'input message:',10d
 	inputMsgLength: equ $-inputMsg
-	kMsg: db 'input k:',10d
+	kMsg: db 'input k (0-25):',10d
 	kMsgLength: equ $-kMsg
 	
 section .text
 	global _start
 	_start:
 	requestInputs:
-		;stdout message requesting message
+		;stdout message requesting message to encrypt
 		mov eax, 4
 		mov ebx, 1
 		mov ecx, inputMsg
@@ -27,6 +27,8 @@ section .text
 		mov ecx, msgBuffer
 		mov edx, msgBuffer_len
 		int 80h
+		mov esi, eax ;save number of bytes read
+		xor ecx, ecx
 	requestK:
 		;stdout message requesting K
 		mov eax, 4
@@ -34,6 +36,9 @@ section .text
 		mov ecx, kMsg
 		mov edx, kMsgLength
 		int 80h
+		movzx eax, byte [kBuffer]
+		sub eax, '0'
+		mov bl, al
 	inputK:
 		;grab stdin for k and push to stack
 		mov eax, 3
@@ -41,34 +46,40 @@ section .text
 		mov ecx, kBuffer
 		mov edx, kBuffer_len
 		int 80h
-		mov ecx, 0
+		mov ecx, ecx
 	encryptMsg:
-	;loop through every byte of msg buffer
-		;filter if upper or lower
-		;check if +K would overflow
-			;if no then add K
-			;if yes then letter becomes 65(upper) or 97(lower)
-		cmp ecx, inputMsgLength
+		cmp ecx, esi ;compare counter with buffer length (esi)
 		jge outputCiphertext
-		mov bl, [msgBuffer+ecx]
-		inc ecx
-		push ebx
+		mov al, [msgBuffer+ecx] ;grab character from buffer
+		push ecx ;save loop counter
 		
-		mov eax,4
-		mov ebx,1
-		mov ecx,esp
-		mov edx,1
-		;int 80h
+		cmp al, 'A'
+		jl notUpper
+		cmp al, 'Z'
+		jg notUpper
+		
+		upperEncrypt:
+			add al, bl ;al += k
+			cmp al, 'Z'+1 ;overflow?
+			jl storeChar
+			sub al, 25 ;wrap: 'Z'+1 -> 'A'
+			jmp storeChar
+		
+		notUpper:
+			add al, bl
+			cmp al, 'z'+1
+			jl storeChar
+			sub al,25
+			jmp storeChar
+		
+		storeChar:
+			mov [msgBuffer+ecx], al
+		
+		pop ecx ;restore loop counter
+		inc ecx
+	jmp encryptMsg
 	
-		jmp encryptMsg
 	
-			lowerEncrypt:
-	
-			jmp encryptMsg
-	
-			upperEncrypt:
-	
-			jmp encryptMsg
 	
 	outputCiphertext:
 		mov eax, 4
